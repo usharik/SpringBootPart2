51dfeabcb6f20edb324091a15677fef389fe713b
usharik <usharik@mail.ru>
2019-06-18 22:47:28 +0200

    Lesson2 Liquibase configuration which could be run by maven plugin or application

--- geek-market-ui/src/main/java/ru/geekmarket/controller/PictureController.java
+++ geek-market-ui/src/main/java/ru/geekmarket/controller/PictureController.java
@@ -3,15 +3,10 @@ package ru.geekmarket.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import ru.geekmarket.persist.model.Picture;
 import ru.geekmarket.persist.repo.PictureRepository;
-import ru.geekmarket.service.ProductService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
