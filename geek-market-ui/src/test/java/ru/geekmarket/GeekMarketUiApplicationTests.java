5cdefabff0763cff059f4aaf8e0eb1ef296415b2
usharik <usharik@mail.ru>
2019-06-21 11:38:18 +0200

    Lesson2 H2 for test running

--- geek-market-ui/src/test/java/ru/geekmarket/GeekMarketUiApplicationTests.java
+++ geek-market-ui/src/test/java/ru/geekmarket/GeekMarketUiApplicationTests.java
@@ -2,15 +2,48 @@ package ru.geekmarket;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.geekmarket.persist.model.Role;
+import ru.geekmarket.persist.model.User;
+import ru.geekmarket.persist.repo.RoleRepository;
+import ru.geekmarket.persist.repo.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class GeekMarketUiApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Test
-    public void contextLoads() {
+    public void testAdminUser() {
+        List<User> users = userRepository.findAll();
+        assertNotNull(users);
+        assertEquals(1, users.size());
+        assertEquals("admin", users.get(0).getUserName());
+        assertTrue(users.get(0).getRoles().contains(new Role("ROLE_ADMIN")));
+    }
+
+    @Test
+    public void testUserRoles() {
+        List<Role> roles = roleRepository.findAll();
+        assertNotNull(roles);
+        assertEquals(2, roles.size());
     }
 
 }
